2ebb0ed79c3316696083e56b6d4582cf478e82b1
David Quisenberry <dmqpdx@gmail.com>
2017-01-30 14:54:32 -0800

    add js code for building province and country objects

--- data/parse_data.js
+++ data/parse_data.js
@@ -1,23 +1,33 @@
+var countriesObjects = [];
+
 $(document).ready(function(){
   var gtags = document.getElementsByTagName('g');
   var countryMap = new Map();
+  var countryNames = [];
   for (var x = 0; x < gtags.length; x++){
-    console.log(gtags[x]);
     var tempString = gtags[x].id;
     var tempCountryProvince = tempString.split(":");
     var tempCountry = tempCountryProvince[0];
-    var tempProvince = tempCountryProvince[1];
+    var tempProvince = new Province (tempCountryProvince[1], tempString);
     if (countryMap.has(tempCountry) === false){
       countryMap.set(tempCountry, [tempProvince]);
+      countryNames.push(tempCountry);
+
     } else {
       countryMap.get(tempCountry).push(tempProvince);
-      console.log(tempCountry);
-      console.log(countryMap.get(tempCountry));
-
+      //console.log(tempCountry);
+      //console.log(countryMap.get(tempCountry));
     }
 
   }
 
+  countryNames.forEach(function(cName){
+    var countryObject = new Country(cName);
+    countryObject.provinces = countryMap.get(cName);
+    countriesObjects.push(countryObject);
+  });
+
+  console.log(countriesObjects);
 
 
 

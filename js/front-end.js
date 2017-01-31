b3dff0a93c93f0fe4b9b3cb3ef8c4a84c9085121
Dylan Stackhouse and Jay Freeman <dylan@>
2017-01-30 16:33:06 -0800

    add disease window and global statistics with basic styling

--- js/front-end.js
+++ js/front-end.js
@@ -1,10 +1,25 @@
 $(function(){
   var red= 1;
-  var green = 2;
+  var green = 2
   var blue = 3;
 
 
   $("g").click(function(){
+    $(this).children("path").css("fill","#"+ red + green + blue);
+    red+=3;
+    green+=2;
+    blue+=1;
+    if(red>=9){
+      red %= 9;
+    }
+    if(blue>=9){
+      blue%= 9;
+    }
+    if(green>=9){
+      green %= 9;
+    }
+  })
+  $("g").each(function(){
     $(this).children("path").css("fill","#"+ red + green + blue);
     red+=3;
     green+=2;

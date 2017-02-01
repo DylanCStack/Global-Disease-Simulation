017f7d76835a7df8210c4647ea9ffdbaeb4afbca
Dylan Stackhouse and Jay Freeman <jay@>
2017-02-01 13:09:59 -0800

    create diseaseManager js file

---
+++ js/diseaseManager.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $("#diseaseInput").submit(function(event) {
+    event.preventDefault();
+
+    //create disease
+
+
+
+    //give disease a starting location
+      //create disease loop/operation which will spread it from the origin point 
+
+
+
+  })
+
+})
